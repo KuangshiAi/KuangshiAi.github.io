9ea5eb2db830f40c5a8f8dfa390b77638df840ff
KuangshiAi <98941044+KuangshiAi@users.noreply.github.com>
2025-10-08 02:10:43 +0000

    Deploying to gh-pages from @ KuangshiAi/KuangshiAi.github.io@8a83fa54cd0ccdc6b7999de9cc81f96fb46bd3a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -81,6 +81,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=PBtq130AAAAJ&hl=en&authuser=1", "_blank");
         },
       },{
+        id: 'social-semanticscholar',
+        title: 'Semantic Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.semanticscholar.org/author/2315113908", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
